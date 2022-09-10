91f03dc2b872623e16774d5c8098cfb6be424f20
Muammer Akca <muammerakca10@gmail.com>
2022-09-10 17:27:24 +0300

    Fixed searching problemwhen prices updated

--- CryptoCurrentPrices/ViewController.swift
+++ CryptoCurrentPrices/ViewController.swift
@@ -11,6 +11,8 @@ class ViewController: UITableViewController, UISearchBarDelegate, FavoriteButton
     
     var timer = Timer()
     
+    var searchingWord = ""
+    
     @IBOutlet var searchBar: UISearchBar!
     
     var cryptosArray = [Crypto]()
@@ -29,13 +31,14 @@ class ViewController: UITableViewController, UISearchBarDelegate, FavoriteButton
         DispatchQueue.global(qos: .userInitiated).async {
             if let url = URL(string: urlString){
                 if let data = try? Data(contentsOf: url){
-                    self.parse(json: data)
+                    self.filteredCryptosArray = self.parse(json: data)
                 } else {
                     self.showError()
                 }
             }
         }
         scheduledTimerWithTimeInterval()
+        self.tableView.reloadData()
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -46,9 +49,6 @@ class ViewController: UITableViewController, UISearchBarDelegate, FavoriteButton
         cell.favoriteButtonForProtocol = self
         //cell.favButton.addTarget(self, action: #selector(favButtonTapped), for: .allEvents)
         cell.indexPath = indexPath
-        
-        
-        
         return cell
     }
     
@@ -64,24 +64,21 @@ class ViewController: UITableViewController, UISearchBarDelegate, FavoriteButton
         }
     }
     
-    func parse(json: Data){
+    func parse(json: Data) -> [Crypto]{
         let decoder = JSONDecoder()
         
         if let jsonCryptos = try? decoder.decode([Crypto].self, from: json) {
             cryptosArray = jsonCryptos
             cryptosArray.sort{$0.symbol < $1.symbol}
-            filteredCryptosArray = cryptosArray
-            //print(jsonCryptos)
-            DispatchQueue.main.async {
-                self.tableView.reloadData()
-            }
+            
         }
+        return cryptosArray
     }
     
     func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
         
         filteredCryptosArray = []
-        
+        searchingWord = searchText
         if searchText == "" {
             filteredCryptosArray = cryptosArray
         }
@@ -104,7 +101,6 @@ class ViewController: UITableViewController, UISearchBarDelegate, FavoriteButton
     }
     
     func scheduledTimerWithTimeInterval(){
-        // Scheduling timer to Call the function "updateCounting" with the interval of 1 seconds
         timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(self.updateCounting), userInfo: nil, repeats: true)
     }
     
@@ -114,7 +110,18 @@ class ViewController: UITableViewController, UISearchBarDelegate, FavoriteButton
         DispatchQueue.global(qos: .userInitiated).async {
             if let url = URL(string: urlString){
                 if let data = try? Data(contentsOf: url){
-                    self.parse(json: data)
+                    self.cryptosArray = self.parse(json: data)
+                    self.filteredCryptosArray = []
+                    if self.searchingWord == "" {
+                        self.filteredCryptosArray = self.cryptosArray
+                    } else {
+                        for crypto in self.cryptosArray {
+                            if crypto.symbol.uppercased().contains(self.searchingWord.uppercased()){
+                                self.filteredCryptosArray.append(crypto)
+                            }
+                        }
+                    }
+                    print(self.filteredCryptosArray)
                 } else {
                     self.showError()
                 }
